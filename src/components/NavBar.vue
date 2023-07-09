5b0ad0bb24c2de0e10112618aa282bbbcb2827e6
Fanny <fannyhedmans@gmail.com>
2023-07-09 21:33:49 +0200

    Added navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,45 @@
 <template>
-  <div>
-    
-  </div>
+  <nav id="navbar">
+    <div id="navbar-container">
+      <h1>Vendre</h1>
+      <p>by optimizers</p>
+    </div>
+  </nav>
 </template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+#navbar {
+  background-color: rgb(255, 255, 255);
+  position: relative;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  top: 0;
+  width: 100%;
+}
+
+#navbar-container {
+  margin: 15px auto 15px 25px;
+  text-align: right;
+}
+
+#navbar-container h1 {
+  margin: 0;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial,
+    sans-serif;
+  font-size: 2.5rem;
+}
+
+#navbar-container p {
+  margin: 0;
+  font-size: 1rem;
+}
+</style>
